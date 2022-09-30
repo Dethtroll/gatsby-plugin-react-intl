682a79b1542a867a368d3ce2248c67a99d1443a3
Aleksey Zhuravlev <brutal.coder@gmail.com>
2022-09-30 15:57:20 +0300

    production buil fix

--- src/util.js
+++ src/util.js
@@ -11,7 +11,8 @@ function removeLocalePart(pathname) {
     return pathname;
   }
 
-  if (window.___gatsbyIntl.languages) {
+  //processing via languages in window
+  if (window && window.___gatsbyIntl.languages) {
 	const languages = window.___gatsbyIntl.languages;
 	
 	var endIndex = pathname.indexOf("/", 1);
@@ -19,7 +20,11 @@ function removeLocalePart(pathname) {
 	return languages.includes(langKey) ? pathname.substring(i) : pathname;
   }
 
+  //processing via two symbol languages
   var i = pathname.indexOf("/", 1);
+  if (i > 3) {
+	return pathname;
+  }
   return pathname.substring(i);
 };
 exports.removeLocalePart = removeLocalePart;
