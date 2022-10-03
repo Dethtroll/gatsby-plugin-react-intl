c4c4350af0b0395ef1c5e451d09ecb14dcebc5c1
Aleksey Zhuravlev <brutal.coder@gmail.com>
2022-10-03 13:06:13 +0300

    production buid fix

--- src/util.js
+++ src/util.js
@@ -7,24 +7,24 @@ function isMatch(patterns, currentPath) {
 exports.isMatch = isMatch
 
 function removeLocalePart(pathname) {
-  if (!window.___gatsbyIntl.routed) {
+  if (typeof window === "undefined" || !window.___gatsbyIntl.routed) {
     return pathname;
   }
 
-  //processing via languages in window
-  if (window && window.___gatsbyIntl.languages) {
-	const languages = window.___gatsbyIntl.languages;
-	
-	var endIndex = pathname.indexOf("/", 1);
-	const langKey = pathname.substring(1, endIndex);
-	return languages.includes(langKey) ? pathname.substring(i) : pathname;
+  //processing via two symbol languages
+  if (typeof window === "undefined" || !window.___gatsbyIntl.languages) {
+  var i = pathname.indexOf("/", 1);
+    if (i > 3) {
+      return pathname;
+    }
+    return pathname.substring(i);
   }
 
-  //processing via two symbol languages
-  var i = pathname.indexOf("/", 1);
-  if (i > 3) {
-	return pathname;
-  }
-  return pathname.substring(i);
+  //processing via languages in window
+  const languages = window.___gatsbyIntl.languages;
+
+  var endIndex = pathname.indexOf("/", 1);
+  const langKey = pathname.substring(1, endIndex);
+  return languages.includes(langKey) ? pathname.substring(endIndex) : pathname;
 };
 exports.removeLocalePart = removeLocalePart;
